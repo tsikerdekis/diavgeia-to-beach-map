ed735a9c9765026dd9a64d2e29ee057022c01b24
tsikerdekis <tsikerdekis@gmail.com>
2023-08-11 09:15:29 -0700

    update

---
+++ parser/get_latlon.py
@@ -0,0 +1,19 @@
+import requests
+
+BING_KEY = "AvU-kQHyioJBmXsT7T0H96XhSNPqxmmJhanur2HxxnF9cdKfHo_bT2RwtlsbWAAR"
+
+def get_latlon(address):
+    """
+    Get the latitude and longitude of an address using the Bing Maps REST API
+    DOES NOT WORK
+    """
+    url = 'https://dev.virtualearth.net/REST/v1/LocalSearch/?query=%s&userLocation=40.07094090125872,23.445713184826324&key=%s' % (address, BING_KEY)
+    print(url)
+    params = {'address': address}
+    r = requests.get(url, params=params)
+    print(r.json())
+    results = r.json()['results']
+    location = results[0]['geometry']['location']
+    return location['lat'], location['lng']
+
+print(get_latlon('coffee'))
